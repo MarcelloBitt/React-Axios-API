2863d496fc4ee0b45d53c7d7c9c07c90f2751b0d
MarcelloBitt <108131718+MarcelloBitt@users.noreply.github.com>
2023-12-13 21:35:11 +0000

    Initial commit

---
+++ src/routes/NewPost.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import blogFetch from "../axios/config";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./NewPost.css";
+
+const NewPost = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ title: "", body: "" });
+  const [titleError, setTitleError] = useState();
+  const [bodyError, setBodyError] = useState();
+
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    if (!formData.title) {
+      setTitleError("Campo obrigatório!");
+      return;
+    }
+    if (!formData.body) {
+      setBodyError("Campo obrigatório!");
+      return;
+    }
+
+    setTitleError("");
+    setBodyError("");
+
+    const title = formData.title;
+    const body = formData.body;
+
+    const post = { title, body, userId: 1 };
+
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+
+    navigate("/");
+  };
+  const handleData = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+    console.log(formData);
+  };
+
+  return (
+    <div className="new-post">
+      <h2>Inserir um novo post</h2>
+      <form onSubmit={(e) => createPost(e)}>
+        <div className="form-control">
+          <label htmlFor="title">Título: </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+            onChange={(e) => {
+              handleData(e);
+              setTitleError("");
+            }}
+          />
+          {titleError && <span className="error-message">{titleError}</span>}
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo: </label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+            onChange={(e) => {
+              handleData(e);
+              setBodyError("");
+            }}
+          ></textarea>
+          {bodyError && <span className="error-message">{bodyError}</span>}
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+    </div>
+  );
+};
+
+export default NewPost;
